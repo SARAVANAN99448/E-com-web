9b2d5cd95c3905f216819849306953c472b6c447
SARAVANAN99448 <saravanasaro0000@gmail.com>
2024-09-25 19:27:08 +0530

    E-commerce website

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy:{
+      '/api': 'https://e-com-web-ujbh.onrender.com/'
+    }
+  },
   plugins: [react()],
 })
