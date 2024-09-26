0022bb1137ac7211554b1c8eb1f9cc93906d1511
SARAVANAN99448 <saravanasaro0000@gmail.com>
2024-09-26 11:16:19 +0530

    changes

--- frontend/src/assets/components/Cart.jsx
+++ frontend/src/assets/components/Cart.jsx
@@ -35,7 +35,6 @@ const Cart = () => {
             });
     }, []);
 
-    const baseURL = "https://e-com-web-ujbh.onrender.com/images/"
 
     return (
         <><Navbar />
@@ -43,11 +42,10 @@ const Cart = () => {
                 <h1 className="text-2xl font-bold text-white mb-5 text-center">Cart Products</h1>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                    
                     {cart.length > 0 ? (
                         cart.map((data, index) => (
                             <div key={index} className="bg-white p-5 rounded-lg shadow-lg flex flex-col items-center">
-                                <img src={`${baseURL}${data.image}`} alt={data.text}
+                                <img src={`${URL}/images/${data.image}`} alt={data.text}
                                     className="w-full h-64 object-cover mb-3" />
                                 <h2 className="text-lg font-semibold mb-2">{data.text}</h2>
                                 <p className="text-green-700 mb-3">{data.price}</p>
