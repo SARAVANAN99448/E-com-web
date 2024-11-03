a025daf2b72603a98758a8aecb4f38da2e11efeb
SARAVANAN99448 <saravanasaro0000@gmail.com>
2024-11-03 12:08:44 +0530

    hero changes

--- frontend/src/assets/components/Hero.jsx
+++ frontend/src/assets/components/Hero.jsx
@@ -5,8 +5,13 @@ const Hero = () => {
     return (<>
     <section className="bg-pink-400">
         <div className="md:flex px-10 py-10 gap-10  justify-center ">
-            <div className="flex md:justify-center md:items-center md:w-1/2 relative">
-                <h1 className="md:text-4xl text-2xl font-bold text-main md:pb-0  pb-10">Become the King of Pirates with Epic One Piece Gear! Discover treasures from the Grand Line. Shop exclusive One Piece merchandise now!</h1>
+            {/* text for desktop */}
+            <div className="md:flex md:justify-center md:items-center md:w-1/2 relative hidden ">
+                <h1 className="md:text-4xl text-2xl font-bold text-main md:pb-0  pb-10">BECOME THE KING OF PIRATES WITH EPIC ONE PIECE GEAR! DISCOVER TREASURES FROM THE GRAND LINE. SHOP EXCLUSIVE ONE PIECE MERCHANDISE NOW!</h1>
+            </div>
+            {/* text for mobile */}
+            <div className=" relative md:hidden text-center">
+                <h1 className="md:text-4xl text-3xl font-bold text-main md:pb-0  pb-10">THE ONE PIECE IS REAL</h1>
             </div>
             <div className="flex justify-center items-center md:pb-0  pb-5">
                 <img src="/images/hero.jpg" alt=""  className="w-96 h-96 md:rounded-full rounded-3xl"/>
